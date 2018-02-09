d672eb305bfdf6a32612912eaa51d829d1c5cc61
Mason Stallmo <masonstallmo@gmail.com>
2018-02-08 23:15:16 -0600

    setup heroku deployment from travis ci

--- tools/distServer.js
+++ tools/distServer.js
@@ -1,10 +1,9 @@
 import express from 'express';
 import path from 'path';
-import open from 'open';
 
 /*eslint-disable no-console*/
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static('dist'));
@@ -13,10 +12,4 @@ app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
-app.listen(port, function(err) {
-    if (err) {
-        console.log(err);
-    } else {
-        open(`http://localhost:${port}`);
-    }
-});
+app.listen(port, () => console.log(`Listening on ${ port }`));
